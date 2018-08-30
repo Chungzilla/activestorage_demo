930454cbaef68093f0357a9d4b0c4ad7f1acbc2f
Chungzilla <chung.jazmine@gmail.com>
2018-08-30 17:15:24 -0400

    updated link to path

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,29 @@
 class PhotosController < ApplicationController
     def index
-        @photo = Photo.new
+        @photo = Photo.all
     end
-end
+    
+    def new
+        @photo = Photo.new
+
+        
+    end
+
+    def show
+        @photo = Photo.find(params[:id])
+    end
+        
+
+    def create
+        @photo = Photo.create(photo_params)
+        redirect_to photos_path
+      end
+
+    end
+      
+    private
+      
+      def photo_params
+        params.require(:photo).permit(:title, :description, :image)
+      end
+    
