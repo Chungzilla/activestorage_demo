268f7a62f9f6fcb9574dcde02a0d426103610f77
Chungzilla <chung.jazmine@gmail.com>
2018-09-03 19:58:25 -0400

    added bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    belongs_to :userable, polymorphic: true
+    belongs_to :house
+    has_many :enrollments
+    has_many :courses, :through => :enrollments
+    has_one_attached :photo
+    validates :first_name, :last_name, :gender, :username, :email, :password, presence: true
+    validates :email, :username, uniqueness: true
+    accepts_nested_attributes_for :userable
+  
+    def self.gender
+      %w[Female Male]
+    end
+  
+    def self.house
+      %w[Baratheon Greyjoy Lannister Martell Stark Targaryen Tyrell]
+    end
+  
+    def full_name
+      "#{first_name}"+" #{last_name}"
+    end
+
 end
--- db/migrate/20180831220302_devise_create_users.rb
+++ db/migrate/20180831220302_devise_create_users.rb
@@ -32,6 +32,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :username
+      t.string :email
+      t.string :password
+      t.references :house
+      t.references :userable, polymorphic: true
 
       t.timestamps null: false
     end
